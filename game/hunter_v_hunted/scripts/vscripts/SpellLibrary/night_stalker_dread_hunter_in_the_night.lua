5759ad3294ef329c676ad461380fd8bfcfdee36a
smithb589 <smithb589@gmail.com>
2015-07-02 19:46:15 -0400

    Adding an applicator script for NS' dread hunter passive.

---
+++ game/hunter_v_hunted/scripts/vscripts/SpellLibrary/night_stalker_dread_hunter_in_the_night.lua
@@ -0,0 +1,11 @@
+function DreadHunterInTheNight(keys)
+	local caster = keys.caster
+	local ability = keys.ability
+	local modifier = keys.modifier
+
+	if not GameRules:IsDaytime() then
+		ability:ApplyDataDrivenModifier(caster, caster, modifier, {})
+	else
+		if caster:HasModifier(modifier) then caster:RemoveModifierByName(modifier) end
+	end
+end
