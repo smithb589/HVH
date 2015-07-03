d19d36ae558aa9fac0032675517da362d3fc508b
smithb589 <smithb589@gmail.com>
2015-07-02 22:21:56 -0400

    Making a courier spawn. He isn't controllable at the moment though...

--- game/hunter_v_hunted/scripts/vscripts/internal/event_handlers.lua
+++ game/hunter_v_hunted/scripts/vscripts/internal/event_handlers.lua
@@ -4,8 +4,8 @@ end
 
 function HVHGameMode:OnRoundStart(gameInfo)
 	print("Creating courier.")
-	local spawner = Entities:GetEntityByName("RadiantCourierSpawner")
-	CreateUnitByName("npc_dota_courier", spawner:GetAbsOrigin(), true, nil, nil, DOTA_TEAM_GOODGUYS)	
+	--local spawner = Entities:GetEntityByName("RadiantCourierSpawner")
+	--CreateUnitByName("npc_dota_courier", spawner:GetAbsOrigin(), true, nil, nil, DOTA_TEAM_GOODGUYS)	
 end
 
 function HVHGameMode:OnPlayerSpawn(playerSpawnArgs)
