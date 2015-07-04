0bd5ca52cec18cddc62b51149575374eea05e332
smithb589 <smithb589@gmail.com>
2015-07-04 10:43:35 -0400

    Restructuring dog AI to fit a behavior model like the one used in the holdout example.

--- game/hunter_v_hunted/scripts/vscripts/ai/ai_dog.lua
+++ game/hunter_v_hunted/scripts/vscripts/ai/ai_dog.lua
@@ -1,14 +1,154 @@
 
-require("../hvh_utils")
+require("ai/ai_core")
+require("hvh_utils")
+
+-- todo: not sure if this needs to be put on the entity since there are multiple
+behaviorSystem = {}
 
 function Spawn( entityKeyValues )
 
-	thisEntity:SetContextThink("ThinkDog", ThinkDog, 1)
-	HVHDebugPrint("Starting AI for "..thisEntity:GetUnitName().." "..thisEntity:GetEntityIndex())
+	thisEntity.Feed = function(feedDuration)
+		thisEntity._FeedTime = GameRules:GetGameTime()
+		thisEntity._FeedDuration = feedDuration
+	end
 
+	thisEntity._WanderingOrigin = Vector(0, 0)
+	thisEntity._FeedDuration = 0
+	-- Arbitraryly age this so the dog doesn't start fed.
+	thisEntity._FeedTime = GameRules:GetGameTime() - 60
+
+	thisEntity:SetContextThink("ThinkDog", ThinkDog, 0.25)
+	behaviorSystem = AICore:CreateBehaviorSystem({
+		BehaviorWander,
+		BehaviorPursue,
+		BehaviorSleep
+	})
+	HVHDebugPrint(string.format("Starting AI for %s. Entity Index: %s", thisEntity:GetUnitName(), thisEntity:GetEntityIndex()))
 end
 
 function ThinkDog()
+	if thisEntity:IsNull() or not thisEntity:IsAlive() then
+		return nil -- deactivate this think function
+	end
+	return behaviorSystem:Think()
+end
+
+--------------------------------------------------------------------------------------------------------
+-- Wander behavior
+
+BehaviorWander = 
+{
+	order = 
+	{
+		UnitIndex = thisEntity:entindex(),
+		OrderType = DOTA_UNIT_ORDER_MOVE_TO_POSITION,
+		Position = thisEntity:GetAbsOrigin()
+	}
+}
+
+function BehaviorWander:Evaluate()
+	-- todo: Determine if not fed and is daytime
+	return 2
+end
+
+function BehaviorWander:Initialize()
+
+end
+
+function BehaviorWander:Begin()
+	self.endTime = GameRules:GetGameTime() + 1
+end
+
+function BehaviorWander:Continue()
+	self.endTime = GameRules:GetGameTime() + 1
+end
+
+function BehaviorWander:End()
+	self.endTime = GameRules:GetGameTime()
+end
+
+function BehaviorWander:Think(dt)
+
+end
+
+--------------------------------------------------------------------------------------------------------
+-- Pursue behavior
+
+BehaviorPursue = 
+{
+	order =
+	{
+		UnitIndex = thisEntity:entindex(),
+		OrderType = DOTA_UNIT_ORDER_MOVE_TO_TARGET,
+
+		-- todo: this is a guess
+		TargetIndex = nil
+	}
+}
+
+function BehaviorPursue:Evaluate()
+	-- todo: determine if fed and is daytime
+	return 1
+end
+
+function BehaviorPursue:Initialize()
+
+end
+
+function BehaviorPursue:Begin()
+	self.endTime = GameRules:GetGameTime() + 1
+end
+
+function BehaviorPursue:Continue()
+	self.endTime = GameRules:GetGameTime() + 1
+end
+
+function BehaviorPursue:End()
+	self.endTime = GameRules:GetGameTime()
+end
+
+function BehaviorPursue:Think(dt)
+
+end
+
+--------------------------------------------------------------------------------------------------------
+-- Sleep behavior
+
+BehaviorSleep = 
+{
+	order = 
+	{
+		UnitIndex = thisEntity:entindex(),
+		OrderType = DOTA_UNIT_ORDER_HOLD_POSITION
+	}
+}
+
+function BehaviorSleep:Evaluate()
+	-- todo: determine if night time
+	return 1
+end
+
+function BehaviorSleep:Initialize()
+
+end
+
+function BehaviorSleep:Begin()
+	self.endTime = GameRules:GetGameTime() + 1
+end
+
+function BehaviorSleep:Continue()
+	self.endTime = GameRules:GetGameTime() + 1
+end
+
+function BehaviorSleep:End()
+	self.endTime = GameRules:GetGameTime()
+end
+
+function BehaviorSleep:Think(dt)
+end
+
+
+--[[function ThinkDog()
 	--print("Thinking...")
 	if GameRules:IsDaytime() then
 
@@ -40,7 +180,7 @@ end
 
 -- returns the closest badguy hero
 function AcquireTarget()
-	HVHDebugPrint("Acquiring target...")
+	--HVHDebugPrint("Acquiring target...")
 	local units = FindUnitsInRadius(DOTA_TEAM_BADGUYS,
 		 	thisEntity:GetAbsOrigin(),
 			nil,
@@ -58,7 +198,7 @@ function AcquireTarget()
 end
 
 function Pursue(target)
-	HVHDebugPrint("Pursuing " .. target:GetUnitName() .. "!")
+	--HVHDebugPrint("Pursuing " .. target:GetUnitName() .. "!")
 	thisEntity:MoveToNPC(target)
 end
 
@@ -68,15 +208,27 @@ end
 
 -- TODO: actual wandering
 function Wander()
-	if not thisEntity:IsIdle() then
-		thisEntity:Stop()
+	if not (thisEntity._IsWandering or false) and not thisEntity:IsIdle() then
+		thisEntity._IsWandering = true
+		local wanderPosition = thisEntity:GetAbsOrigin()
+		local wanderDelta = RandomVector(300)
+		wanderPosition = wanderPosition + wanderDelta
+
+		--local finalDestination = FindClearSpaceForUnit(thisEntity, wanderPosition, false) 
+
+		thisEntity:MoveToPosition(wanderPosition)
+	else
+		HVHDebugPrint("Already wandering.")
 	end
-	HVHDebugPrint("Wandering...")
+
+
+	--HVHDebugPrint("Wandering...")
 end
 
 function Sleep()
 	if not thisEntity:IsIdle() then
 		thisEntity:Stop()
 	end
-	HVHDebugPrint("Sleeping...")
+	--HVHDebugPrint("Sleeping...")
 end
+]]
