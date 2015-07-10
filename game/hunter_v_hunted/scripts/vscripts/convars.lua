8a1e66c52931002745ec610d65f2c81ffb31e5e2
veggiesama <veggiesama@gmail.com>
2015-07-10 14:34:25 -0400

    hvh_fake_heroes console command
    Loads up missing players as bots controllable by the console commander

--- game/hunter_v_hunted/scripts/vscripts/convars.lua
+++ game/hunter_v_hunted/scripts/vscripts/convars.lua
@@ -1,5 +1,3 @@
-
-
 if HVHConvars == nil then
   HVHConvars = class({})
 end
@@ -28,4 +26,36 @@ function HVHConvars:RegisterCommands()
 
   	-- this is already built into the engine
   	--Convars:RegisterCommand( "set_time_of_day", Dynamic_Wrap(self, 'ConvarSetTimeOfDay'), "Sets the time of day to the indicated value.", FCVAR_CHEAT )
+    Convars:RegisterCommand( "hvh_fake_heroes", Dynamic_Wrap(self, 'FakeHeroes'), "Spawn heroes to fill in missing players.", FCVAR_CHEAT )
+end
+
+function HVHConvars:FakeHeroes()
+  local empty_gg = CUSTOM_TEAM_PLAYER_COUNT[DOTA_TEAM_GOODGUYS] - PlayerResource:GetPlayerCountForTeam(DOTA_TEAM_GOODGUYS) 
+  local empty_bg = CUSTOM_TEAM_PLAYER_COUNT[DOTA_TEAM_BADGUYS]  - PlayerResource:GetPlayerCountForTeam(DOTA_TEAM_BADGUYS)
+  local humanPlayer = Convars:GetCommandClient()
+  local humanPlayerID = humanPlayer:GetPlayerID()
+
+  -- spawn missing good guys
+  while empty_gg ~= 0 do
+    --local spawner = HVHGameMode:ChooseRandomSpawn("info_player_start_goodguys")
+    --local position = spawner:GetAbsOrigin()
+    --local hero = CreateUnitByName("npc_dota_hero_sniper", position, true, nil, nil, DOTA_TEAM_GOODGUYS)
+    local msg = "dota_create_unit npc_dota_hero_sniper"
+    if PlayerResource:GetTeam(humanPlayerID) ~= DOTA_TEAM_GOODGUYS then
+      msg = msg .. " enemy"
+    end
+     SendToServerConsole(msg)
+    empty_gg = empty_gg - 1
+  end
+
+  -- spawn missing bad guys
+  while empty_bg ~= 0 do
+    local msg = "dota_create_unit npc_dota_hero_night_stalker"
+    if PlayerResource:GetTeam(humanPlayerID) ~= DOTA_TEAM_BADGUYS then
+      msg = msg .. " enemy"
+    end
+    SendToServerConsole(msg)
+    empty_bg = empty_bg - 1
+  end
+
 end
