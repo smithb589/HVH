23530ff4163b7cc6213a23a6636af12b3d5f6da5
veggiesama <veggiesama@gmail.com>
2015-09-15 20:32:45 -0400

    Externalized strings in power_stages.lua and tutorial.lua

--- game/hunter_v_hunted/scripts/vscripts/internal/power_stages.lua
+++ game/hunter_v_hunted/scripts/vscripts/internal/power_stages.lua
@@ -7,20 +7,14 @@ NS_LANDSCAPE_PATH = "file://{images}/custom_game/notifications/nightstalker_land
 
 NOTIFICATION_DURATION = 6.5
 
-STR_SNIPERS_UPGRADE = "Reinforcements have arrived: "
-STR_SNIPERS_EARTHBIND   = "MEEPO'S NETS!"
-STR_SNIPERS_TIMBERCHAIN = "TIMBER CHAINS!"
-STR_SNIPERS_BONUSHOUNDS = "THE PACK GROWS!"
-STR_SNIPERS_SHRAPNEL	= "+1 MAX SHRAPNEL CHARGE" 
-STR_NS_UPGRADE 		 = "The Night Stalker has evolved: "
-STR_NS_LEAP          = "DREAD LEAP!"
-STR_NS_ECHOLOCATION  = "ECHOLOCATION!"
-STR_NS_CRIPPLE_AOE   = "CRIPPLING HYSTERIA (AOE)"
-
+-- Power Stages convey benefits to your entire team based on your team's lives remaining
 function HVHPowerStages:Setup()
 	ListenToGameEvent("entity_killed", Dynamic_Wrap(self, "OnEntityKilled"), self)
 	self.gg_stage = 0
 	self.bg_stage = 0
+
+	-- English-only right now
+	self.localization = LoadKeyValues("resource/addon_english.txt")["Tokens"]["HVH_PowerStages"]
 end
 
 function HVHPowerStages:OnEntityKilled(killedArgs)
@@ -54,9 +48,11 @@ function HVHPowerStages:CheckForWinner()
 	local mode = GameRules:GetGameModeEntity()
 	if mode.GoodGuyLives <= 0 then
 		GameRules:SetSafeToLeave( true )
+		self:NotifyVictory( DOTA_TEAM_BADGUYS )		
 		GameRules:SetGameWinner( DOTA_TEAM_BADGUYS )
 	elseif mode.BadGuyLives <= 0 then
 		GameRules:SetSafeToLeave( true )
+		self:NotifyVictory( DOTA_TEAM_GOODGUYS )
 		GameRules:SetGameWinner( DOTA_TEAM_GOODGUYS )
 	end
 end
@@ -100,8 +96,9 @@ function HVHPowerStages:UpgradePowerStage(team)
     end)
 end
 
--- improves an ability to by a single level for all heroes matching hero_name
--- removes old version of ability if replaced_ability_name is used
+-- improves an ability by a single level for all heroes matching hero_name
+-- optional: old ability "replaced_ability_name" can be removed
+-- optional: "maxout" will level the ability up to the maximum level
 function HVHPowerStages:LevelupAbilityForAll(hero_name, ability_name, replaced_ability_name, maxout)
     local heroList = HeroList:GetAllHeroes()
     for _,hero in pairs(heroList) do
@@ -117,68 +114,54 @@ function HVHPowerStages:LevelupAbilityForAll(hero_name, ability_name, replaced_a
     end
 end
 
+--------------------------------------------------------- GRANT ABILITIES AND PRINT NOTIFICATIONS
 function HVHPowerStages:GrantSniperShrapnelCharge()
 	-- short delay before posting
 	Timers:CreateTimer(NOTIFICATION_DURATION, function()
 		self:LevelupAbilityForAll("npc_dota_hero_sniper", "sniper_shrapnel_hvh", nil, false)
-		self:Notify(STR_SNIPERS_UPGRADE, STR_SNIPERS_SHRAPNEL, "", DOTA_TEAM_GOODGUYS)
+		self:Notify(self.localization["Snipers_Upgrade"], self.localization["Snipers_Shrapnel"], "", DOTA_TEAM_GOODGUYS)
 		return nil
 	end)
 end
 
 function HVHPowerStages:GrantSniperEarthbind()
 	self:LevelupAbilityForAll("npc_dota_hero_sniper", "meepo_earthbind", nil, true)
-	self:Notify(STR_SNIPERS_UPGRADE, STR_SNIPERS_EARTHBIND, "meepo_earthbind", DOTA_TEAM_GOODGUYS)
+	self:Notify(self.localization["Snipers_Upgrade"], self.localization["Snipers_Earthbind"], "meepo_earthbind", DOTA_TEAM_GOODGUYS)
 end
 
 function HVHPowerStages:GrantSniperTimberchain()
 	self:LevelupAbilityForAll("npc_dota_hero_sniper", "shredder_timber_chain", nil, true)
-	self:Notify(STR_SNIPERS_UPGRADE, STR_SNIPERS_TIMBERCHAIN, "shredder_timber_chain", DOTA_TEAM_GOODGUYS)
+	self:Notify(self.localization["Snipers_Upgrade"], self.localization["Snipers_Timberchain"], "shredder_timber_chain", DOTA_TEAM_GOODGUYS)
 end
 
 function HVHPowerStages:GrantSniperBonusHounds()
 	HVHGameMode:SpawnDog(true)
 	HVHGameMode:SpawnDog(true)
-	self:Notify(STR_SNIPERS_UPGRADE, STR_SNIPERS_BONUSHOUNDS, "lycan_summon_wolves", DOTA_TEAM_GOODGUYS)
+	self:Notify(self.localization["Snipers_Upgrade"], self.localization["Snipers_BonusHounds"], "lycan_summon_wolves", DOTA_TEAM_GOODGUYS)
 end
 
 function HVHPowerStages:GrantNSLeap()
 	self:LevelupAbilityForAll("npc_dota_hero_night_stalker", "night_stalker_leap_hvh", nil, true)
-	self:Notify(STR_NS_UPGRADE, STR_NS_LEAP, "night_stalker_leap_hvh", DOTA_TEAM_BADGUYS)
+	self:Notify(self.localization["NS_Upgrade"], self.localization["NS_Leap"], "night_stalker_leap_hvh", DOTA_TEAM_BADGUYS)
 end
 
 function HVHPowerStages:GrantNSEcholocation()
 	self:LevelupAbilityForAll("npc_dota_hero_night_stalker", "night_stalker_echolocation_hvh", nil, true)
-	self:Notify(STR_NS_UPGRADE, STR_NS_ECHOLOCATION, "night_stalker_echolocation_hvh", DOTA_TEAM_BADGUYS)
+	self:Notify(self.localization["NS_Upgrade"], self.localization["NS_Echolocation"], "night_stalker_echolocation_hvh", DOTA_TEAM_BADGUYS)
 end
 
 function HVHPowerStages:GrantNSCripplingFearAOE()
 	self:LevelupAbilityForAll("npc_dota_hero_night_stalker", "night_stalker_crippling_fear_aoe_hvh", "night_stalker_crippling_fear_hvh", true)
-	self:Notify(STR_NS_UPGRADE, STR_NS_CRIPPLE_AOE, "night_stalker_crippling_fear_aoe_hvh", DOTA_TEAM_BADGUYS)
+	self:Notify(self.localization["NS_Upgrade"], self.localization["NS_CripplingHysteria"], "night_stalker_crippling_fear_aoe_hvh", DOTA_TEAM_BADGUYS)
 end
 
 function HVHPowerStages:Notify(heading, subtext, ability_name, team)
-	local teamColor = nil
-	local imagePath = nil
-	if team == DOTA_TEAM_GOODGUYS then
-		teamColor = SNIPERS_COLOR_HEX
-		imagePath = SNIPERS_LANDSCAPE_PATH
-	else
-		teamColor = NS_COLOR_HEX
-		imagePath = NS_LANDSCAPE_PATH
-	end
+	local teamColor = self:GetTeamColor(team)
+	local imagePath = self:GetImagePath(team)
 
-	local cssStyleHeading = {
-		["color"] = teamColor,
-		["font-size"] = "28px",
-		["background-color"] = "#000000", -- black
-		["padding"] ="5px 20px 5px 20px"
-	}
-
-	local cssStyleGained = {
-		["color"] = teamColor,
-		["font-size"] ="48px"
-	}
+	local cssTable = self:GetCSS(teamColor)
+	local cssStyleHeading = cssTable["heading"]
+	local cssStyleGained = cssTable["gained"]
 
 	Notifications:TopToAll({text=heading..subtext, duration=NOTIFICATION_DURATION, style=cssStyleHeading})
 
@@ -200,6 +183,7 @@ function HVHPowerStages:Notify(heading, subtext, ability_name, team)
 	end
 end
 
+----------------------------------------------------------- MISC. FUNCTIONS
 function HVHPowerStages:IncrementPowerStageNumber(team)
 	local newStage = nil
 
@@ -229,4 +213,67 @@ function HVHPowerStages:GetTeamVariables(team)
 	end
 
 	return keys
+end
+
+function HVHPowerStages:GetImagePath(team)
+	if team == DOTA_TEAM_GOODGUYS then
+		return SNIPERS_LANDSCAPE_PATH
+	else
+		return NS_LANDSCAPE_PATH
+	end
+end
+
+function HVHPowerStages:GetTeamColor(team)
+	if team == DOTA_TEAM_GOODGUYS then
+		return SNIPERS_COLOR_HEX
+	else
+		return NS_COLOR_HEX
+	end
+end
+
+function HVHPowerStages:GetCSS(teamColor)
+	local cssStyleHeading = {
+		["color"] = teamColor,
+		["font-size"] = "28px",
+		["background-color"] = "#000000", -- black
+		["padding"] ="5px 20px 5px 20px"
+	}
+
+	local cssStyleGained = {
+		["color"] = teamColor,
+		["font-size"] ="48px"
+	}
+
+	local cssTable = {}
+	cssTable["heading"] = cssStyleHeading
+	cssTable["gained"] = cssStyleGained
+	return cssTable
+end
+
+------------------------------------------------------ BROKEN FUNCTIONS
+-- BUG: panorama notifications do not seem to function post-victory, so this function won't work
+function HVHPowerStages:NotifyVictory(team)
+	local teamColor = self:GetTeamColor(team)
+	local imagePath = self:GetImagePath(team)
+
+	local cssTable = self:GetCSS(teamColor)
+	local cssStyleHeading = cssTable["heading"]
+
+	Notifications:BottomToAll({image=imagePath, duration=NOTIFICATION_DURATION})
+	Notifications:BottomToAll({text="VICTORY", duration=NOTIFICATION_DURATION, style=cssStyleHeading, continue=true})
+	Notifications:BottomToAll({image=imagePath, duration=NOTIFICATION_DURATION, continue=true})
+end
+
+-- BUG: borrowed from Noya's dotacraft; does not seem to work either
+function HVHPowerStages:PrintWinMessageForTeam( teamID )
+	for playerID = 0, DOTA_MAX_TEAM_PLAYERS do
+		if PlayerResource:IsValidPlayerID(playerID) then
+			local player = PlayerResource:GetPlayer(playerID)
+			if player:GetTeamNumber() == teamID then
+				local playerName = PlayerResource:GetPlayerName(playerID)
+				if playerName == "" then playerName = "Player "..playerID end
+				GameRules:SendCustomMessage(playerName .. " was victorious!", 0, 0)
+			end
+		end
+	end
 end
--- game/hunter_v_hunted/scripts/vscripts/internal/tutorial.lua
+++ game/hunter_v_hunted/scripts/vscripts/internal/tutorial.lua
@@ -2,41 +2,25 @@ if HVHTutorial == nil then
 	HVHTutorial = class({})
 end
 
-STR_TUTORIAL_GG = {
-	"You are a SNIPER.",
-	"During the day, you must HUNT the Night Stalker.",
-	"During the night, you must RUN from the Night Stalker and collect ITEMS!",
-	"FOLLOW THE HOUND!",
-	"The hound's aura will protect and heal you, but only during the day.",
-	"Now... HUNT!"
-}
+TUTORIAL_DELAY = 4.8
 
-STR_TUTORIAL_BG = {
-	"You are the NIGHT STALKER.",
-	"During the day, you must RUN from the Snipers and collect ITEMS!",
-	"During the night, you must HUNT the Snipers.",
-	"BEWARE THE HOUND!",
-	"Becoming invisible will regenerate you, but the Hound can still find you.",
-	"Now... RUN!"
-}
-
+-- Print messages to the player at the start of the match
 function HVHTutorial:Start(playerID)
 	local tutorialStrings = {}
 	local colorHex = ""
 	if PlayerResource:GetTeam(playerID) == DOTA_TEAM_GOODGUYS then
-		tutorialStrings = STR_TUTORIAL_GG
+		tutorialStrings = LoadKeyValues("resource/addon_english.txt")["Tokens"]["HVH_Tutorial_Snipers"]
 		colorHex = SNIPERS_COLOR_HEX
 	else
-		tutorialStrings = STR_TUTORIAL_BG
+		tutorialStrings = LoadKeyValues("resource/addon_english.txt")["Tokens"]["HVH_Tutorial_NS"]
 		colorHex = NS_COLOR_HEX
 	end
 
-	TUTORIAL_DELAY = 4.8
 	local i = 1 -- Lua arrays start at 1
 	Timers:CreateTimer(TUTORIAL_DELAY, function()
-		Notifications:Top(playerID, {text=tutorialStrings[i], duration=TUTORIAL_DELAY, style={color=colorHex}})
+		Notifications:Top(playerID, {text=tutorialStrings["MSG_"..i], duration=TUTORIAL_DELAY, style={color=colorHex}})
 		i = i + 1
-		if tutorialStrings[i] ~= nil then
+		if tutorialStrings["MSG_"..i] ~= nil then
 			return TUTORIAL_DELAY
 		else
 			return nil
