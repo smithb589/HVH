fd836d20c597d00db1eec1272e0100011b18510d
veggiesama <veggiesama@gmail.com>
2016-08-17 16:01:44 -0400

    Sunshard targeting fixes

--- game/hunter_v_hunted/scripts/vscripts/spells/item_sun_shard_hvh.lua
+++ game/hunter_v_hunted/scripts/vscripts/spells/item_sun_shard_hvh.lua
@@ -1,24 +1,29 @@
-function SunShard_Use(keys)
+function SunShard_OnUse(keys)
 	local caster = keys.caster
 	local target = keys.target
 	local item = keys.ability
 
-	local charges = item:GetCurrentCharges()
-	local seconds = charges * 3.0
+	if not HVHPhoenix:IsPhoenix(target) and not HVHPhoenix:IsEgg(target) then
+		print("Invalid target")
+		return
+	end
+
 	local mode = GameRules:GetGameModeEntity()
-	--print("Spending "..charges.." charges to add/subtract "..seconds.." seconds.")
 
 	if GameRules:IsDaytime() then
-		mode.HalfCycleTimeRemaining = mode.HalfCycleTimeRemaining + seconds
+		mode.HalfCycleTimeRemaining = mode.HalfCycleTimeRemaining + SUN_SHARD_BONUS_TIME
 	else
-		mode.HalfCycleTimeRemaining = mode.HalfCycleTimeRemaining - seconds
+		mode.HalfCycleTimeRemaining = mode.HalfCycleTimeRemaining - SUN_SHARD_BONUS_TIME
 	end		
 
-	caster:EmitSound("DOTA_Item.MedallionOfCourage.Activate")
-	
-	Timers:CreateTimer(0.1, function()
-		caster:RemoveItem(item)
-	end)
+	--TODO: create tracking particle
+
+	ParticleManager:CreateParticle("particles/ui/ui_generic_treasure_impact.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
+	ParticleManager:CreateParticle("particles/units/heroes/hero_omniknight/omniknight_purification.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+	target:EmitSound("Hero_Omniknight.Purification")
+
+	HVHItemUtils:ExpendCharge(item)
+
 end
 
 function SunShard_OnDeath(keys)
@@ -38,3 +43,38 @@ function SunShard_OnDeath(keys)
 		caster:RemoveItem(item)
 	end)
 end
+
+--[[
+   	"OnSpellStart"
+	{
+		"TrackingProjectile"
+		{
+			"Target" 		"TARGET"
+		    "EffectName"  	"particles/thrown_sunshard.vpcf"
+		    "MoveSpeed"   	"750"
+		    "StartPosition" "attach_origin"
+		}
+
+		"FireSound"
+		{
+			"EffectName"	"Hero_ChaosKnight.idle_throw"
+			"Target" 		"CASTER"
+		}
+	}
+
+	"OnProjectileHitUnit"
+	{
+		"RunScript"
+		{
+			"Target"		"TARGET"
+			"ScriptFile"	"spells/item_sun_shard_hvh.lua"
+			"Function"		"SunShard_OnHit"
+		}
+
+		"FireSound"
+		{
+			"EffectName"	"DOTA_Item.MedallionOfCourage.Activate"
+			"Target" 		"TARGET"
+		}
+	}
+--]]
