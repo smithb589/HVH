abffdb45858bd4cdcf3d3657f2fc64de5a14c522
veggiesama <veggiesama@gmail.com>
2017-10-24 22:59:53 -0400

    Incremented version to 1.6.7

--- game/hunter_v_hunted/scripts/vscripts/hvh_settings.lua
+++ game/hunter_v_hunted/scripts/vscripts/hvh_settings.lua
@@ -1,5 +1,5 @@
 -- In this file you can set up all the properties and settings for your game mode.
-HVH_VERSION 		= "1.6.6"
+HVH_VERSION 		= "1.6.7"
 
 -- HVH Leveling and Abilities
 STARTING_LEVEL		= 16
