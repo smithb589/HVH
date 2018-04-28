61ad6ab32a6887c1c6a67f267b90c038feeac4ab
veggiesama <veggiesama@gmail.com>
2018-04-27 21:56:39 -0400

    Fix for sun shards used too quickly

--- game/hunter_v_hunted/scripts/vscripts/utils/item_utils.lua
+++ game/hunter_v_hunted/scripts/vscripts/utils/item_utils.lua
@@ -53,6 +53,7 @@ function HVHItemUtils:ExpendCharge(item)
     item:SetCurrentCharges(newCharges)
 
 	Timers:CreateTimer(0.25, function() -- SINGLE_FRAME_TIME too short for item_manta
+		if item:IsNull() then return end
 		if item:GetCurrentCharges() <= 0 then -- recheck in case the charge was refunded
 	    	hero:RemoveItem(item)
 	    end
